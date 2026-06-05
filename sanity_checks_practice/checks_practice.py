c68eb16fd3d31ac89b338473172cb8725b767ae5
Brandon Knight <Brandon.Knight@bunzlusa.com>
2026-06-05 11:31:43 -0500

    Adding more notes/comments to checks.py and checks_practice.py. Learning how this program works..

---
+++ sanity_checks_practice/checks_practice.py
@@ -0,0 +1,61 @@
+
+
+# 1. import the necessary libraries:
+#    - pandas for data manipulaiton
+#    - 'from' schema python file -> import schema dict
+
+# 2. Is number? (bool function): 
+#       - Check if value is NA, return True (we allow empty cells in numeric columns)
+#       - Check if value is bool, return False (we don't want to allow True/False values in numeric columns)
+#       - Check if value is int or float, return True (we want to allow numbers in numeric columns)
+#       - Return true if value is int or float, false otherwise (we don't want to allow strings or other types in numeric columns)
+
+
+# 3. Is string? (bool function):
+#       - Check if value is NA, return True (we allow empty cells in string columns)
+#       - Check if value is int, float, or bool, return False (we don't want to allow numbers or True/False values in string columns)
+#       - Return True if value is str, false otherwise (we want to allow strings in string columns, but not other types)
+
+# 4. Is date? (bool function):
+
+# 5. Is boolean? (bool function): This one isn't even really neccesary for this project
+
+
+# 5. All in rate check function
+
+# 6. Column type check function
+
+# 7. Check all in totals are greater than zero function
+
+# 8. Function to validate all effective dates are less than end dates
+
+# 9. Returns dataframe ??
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
